b43145f4a478911586f192b73d6b661e6e4c2060
MrGu00 <michaeljcnuri@gmail.com>
2026-04-22 18:07:49 +0700

    field property & cons default users & messages

---
+++ chatServer/Messages.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chatServer
+{
+    class Messages
+    {
+
+        string messagesId;
+        string sender;
+        string receiver;
+        string message;
+        DateTime sendedAt;
+
+        public Messages(string messagesId, string sender, string receiver, string messages, DateTime sendedAt)
+        {
+            this.MessagesId = messagesId;
+            this.Sender = sender;
+            this.Receiver = receiver;
+            this.Message = message;
+            this.SendedAt = sendedAt;
+        }
+
+        public string MessagesId { get => messagesId; set => messagesId = value; }
+        public string Sender { get => sender; set => sender = value; }
+        public string Receiver { get => receiver; set => receiver = value; }
+        public string Message { get => message; set => message = value; }
+        public DateTime SendedAt { get => sendedAt; set => sendedAt = value; }
+    }
+}
---
+++ chatServer/Users.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chatServer
+{
+    class Users
+    {
+
+        int userId;
+        string username;
+        string password;
+        DateTime createdAt;
+        string role;
+
+        public Users(int userId, string username, string password, DateTime createdAt, string role)
+        {
+            this.UserId = userId;
+            this.Username = username;
+            this.Password = password;
+            this.CreatedAt = createdAt;
+            this.Role = role;
+        }
+
+        public int UserId { get => userId; set => userId = value; }
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+        public DateTime CreatedAt { get => createdAt; set => createdAt = value; }
+        public string Role { get => role; set => role = value; }
+    }
+}
